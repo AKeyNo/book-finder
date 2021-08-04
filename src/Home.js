20c00f1da3ba137b13353529b81c6b1fd6800176
AKeyNo <arielaquino.cs@gmail.com>
2021-08-03 20:53:13 -0700

    added images

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export const Home = () => {
   const [search, setSearch] = useState(null);
   const [book, setBook] = useState(null);
 
   useEffect(() => {
-    console.log('Hello there.');
+    console.log("Hello there.");
   }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     axios
-      .get('https://www.googleapis.com/books/v1/volumes?q=' + search)
+      .get("https://www.googleapis.com/books/v1/volumes?q=" + search)
       .then((data) => {
         console.log(data.data.items);
-        setBook(data.data.items[0].volumeInfo);
+        console.log(data);
+        if (data.data.totalItems > 0) {
+          setBook(data.data.items[0].volumeInfo);
+          // console.log(book.imageLinks);
+        } else {
+          setBook(null);
+          window.alert(search + " did not return any results.");
+        }
+      })
+      .catch((error) => {
+        window.alert(error);
       });
   };
 
   const BookInformation = () => {
-    return <>{book.title}</>;
+    return (
+      <div>
+        <img
+          src={
+            book.imageLinks !== undefined
+              ? book.imageLinks.thumbnail
+              : "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Missing.png/440px-Missing.png"
+          }
+        />
+      </div>
+    );
   };
 
   return (
@@ -30,12 +50,12 @@ export const Home = () => {
         <label>
           Book:
           <input
-            type='text'
-            name='title'
+            type="text"
+            name="title"
             onChange={(e) => setSearch(e.target.value)}
           />
         </label>
-        <input type='submit' value='Search' />
+        <input type="submit" value="Search" />
       </form>
       {book && <BookInformation />}
     </>
