80534d0b6d3cde6b0fa582207cc7704718c75ed9
AKeyNo <arielaquino.cs@gmail.com>
2021-08-03 21:04:52 -0700

    loading for images and changed quotations into single quotes

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,20 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export const Home = () => {
   const [search, setSearch] = useState(null);
   const [book, setBook] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    console.log("Hello there.");
+    console.log('Hello there.');
   }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setLoading(true);
     axios
-      .get("https://www.googleapis.com/books/v1/volumes?q=" + search)
+      .get('https://www.googleapis.com/books/v1/volumes?q=' + search)
       .then((data) => {
         console.log(data.data.items);
         console.log(data);
@@ -21,11 +23,14 @@ export const Home = () => {
           // console.log(book.imageLinks);
         } else {
           setBook(null);
-          window.alert(search + " did not return any results.");
+          window.alert(search + ' did not return any results.');
         }
       })
       .catch((error) => {
         window.alert(error);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   };
 
@@ -36,8 +41,9 @@ export const Home = () => {
           src={
             book.imageLinks !== undefined
               ? book.imageLinks.thumbnail
-              : "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Missing.png/440px-Missing.png"
+              : 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Missing.png/440px-Missing.png'
           }
+          alt={book.title}
         />
       </div>
     );
@@ -50,14 +56,14 @@ export const Home = () => {
         <label>
           Book:
           <input
-            type="text"
-            name="title"
+            type='text'
+            name='title'
             onChange={(e) => setSearch(e.target.value)}
           />
         </label>
-        <input type="submit" value="Search" />
+        <input type='submit' value='Search' />
       </form>
-      {book && <BookInformation />}
+      {!loading ? book && <BookInformation /> : 'loading'}
     </>
   );
 };
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './index.css';
+// import styles from './index.css';
 import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
