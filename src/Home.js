30cbe923ced34e710a556bdc984a4a65e191e40d
AKeyNo <arielaquino.cs@gmail.com>
2021-08-04 21:08:58 -0700

    paper

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import axios from 'axios';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Paper from "@material-ui/core/Paper";
 
 export const Home = () => {
   const [search, setSearch] = useState(null);
@@ -10,14 +11,14 @@ export const Home = () => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    console.log('Hello there.');
+    console.log("Hello there.");
   }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     setLoading(true);
     axios
-      .get('https://www.googleapis.com/books/v1/volumes?q=' + search)
+      .get("https://www.googleapis.com/books/v1/volumes?q=" + search)
       .then((data) => {
         console.log(data.data.items);
         console.log(data);
@@ -26,7 +27,7 @@ export const Home = () => {
           // console.log(book.imageLinks);
         } else {
           setBook(null);
-          window.alert(search + ' did not return any results.');
+          window.alert(search + " did not return any results.");
         }
       })
       .catch((error) => {
@@ -37,31 +38,33 @@ export const Home = () => {
         setLoading(false);
       });
   };
+  //9x9 grid or something with a page next
 
   const BookInformation = useCallback(() => {
     return (
-      <div>
+      <Paper elevation={0} variant="outlined">
         <img
           src={
             book.imageLinks !== undefined
               ? book.imageLinks.thumbnail
-              : 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Missing.png/440px-Missing.png'
+              : "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Missing.png/440px-Missing.png"
           }
           alt={book.title}
         />
-      </div>
+        <div>{book.title}</div>
+      </Paper>
     );
   }, [book]);
 
   return (
     <>
-      <form onSubmit={handleSubmit} noValidate autoComplete='off'>
+      <form onSubmit={handleSubmit} noValidate autoComplete="off">
         <TextField
-          id='standard-basic'
-          label='Enter book name'
+          id="standard-basic"
+          label="Enter book name"
           onChange={(e) => setSearch(e.target.value)}
         />
-        <Button type='submit'>Submit</Button>
+        <Button type="submit">Submit</Button>
       </form>
       {!loading ? book && <BookInformation /> : <CircularProgress />}
     </>
