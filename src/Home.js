611db54aa590527ee23ed0d2400e210d07d65245
AKeyNo <arielaquino.cs@gmail.com>
2021-08-04 23:37:12 -0700

    mapping to multiple books on the main page

--- src/Home.js
+++ src/Home.js
@@ -1,38 +1,44 @@
-import React, { useState, useEffect, useCallback } from "react";
-import axios from "axios";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import Paper from "@material-ui/core/Paper";
+import React, { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles({
+  root: {},
+});
 
 export const Home = () => {
+  const classes = useStyles();
   const [search, setSearch] = useState(null);
-  const [book, setBook] = useState(null);
+  const [books, setBooks] = useState(null);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    console.log("Hello there.");
+    console.log('Hello there.');
   }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     setLoading(true);
     axios
-      .get("https://www.googleapis.com/books/v1/volumes?q=" + search)
+      .get('https://www.googleapis.com/books/v1/volumes?q=' + search)
       .then((data) => {
-        console.log(data.data.items);
-        console.log(data);
+        console.log(data.data);
         if (data.data.totalItems > 0) {
-          setBook(data.data.items[0].volumeInfo);
+          setBooks(data.data.items);
           // console.log(book.imageLinks);
         } else {
-          setBook(null);
-          window.alert(search + " did not return any results.");
+          setBooks(null);
+          window.alert(search + ' did not return any results.');
         }
       })
       .catch((error) => {
         window.alert(error);
-        setBook(null);
+        setBooks(null);
       })
       .finally(() => {
         setLoading(false);
@@ -42,31 +48,46 @@ export const Home = () => {
 
   const BookInformation = useCallback(() => {
     return (
-      <Paper elevation={0} variant="outlined">
-        <img
-          src={
-            book.imageLinks !== undefined
-              ? book.imageLinks.thumbnail
-              : "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Missing.png/440px-Missing.png"
-          }
-          alt={book.title}
-        />
-        <div>{book.title}</div>
-      </Paper>
+      <Grid
+        container
+        direction='row'
+        justifyContent='center'
+        alignItems='center'
+        spacing={3}
+      >
+        {books.map((book) => {
+          const bookInformation = book.volumeInfo;
+          return (
+            <Grid item xs>
+              <Paper elevation={0} variant='outlined'>
+                <img
+                  src={
+                    bookInformation.imageLinks !== undefined
+                      ? bookInformation.imageLinks.thumbnail
+                      : 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Missing.png/440px-Missing.png'
+                  }
+                  alt={bookInformation.title}
+                />
+                <div>{bookInformation.title}</div>
+              </Paper>
+            </Grid>
+          );
+        })}
+      </Grid>
     );
-  }, [book]);
+  }, [books]);
 
   return (
     <>
-      <form onSubmit={handleSubmit} noValidate autoComplete="off">
+      <form onSubmit={handleSubmit} noValidate autoComplete='off'>
         <TextField
-          id="standard-basic"
-          label="Enter book name"
+          id='standard-basic'
+          label='Enter book name'
           onChange={(e) => setSearch(e.target.value)}
         />
-        <Button type="submit">Submit</Button>
+        <Button type='submit'>Submit</Button>
       </form>
-      {!loading ? book && <BookInformation /> : <CircularProgress />}
+      {!loading ? books && <BookInformation /> : <CircularProgress />}
     </>
   );
 };
