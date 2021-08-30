2f9f292ec64fa3b3d931fe33b494b4c9da1bfa1e
AKeyNo <arielaquino.cs@gmail.com>
2021-08-30 01:30:47 -0700

    Add to booksRouter getting specific book by id and remove comment from BookPage
    Get returns the volumeinfo if possible from the respective id.

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -1,4 +1,22 @@
 require('dotenv').config();
 const booksRouter = require('express').Router();
+const axios = require('axios');
+
+booksRouter.get('/:book_id', async (request, response) => {
+  const { book_id } = request.params;
+
+  try {
+    const results = await axios
+      .get(`https://www.googleapis.com/books/v1/volumes/${book_id}`)
+      .catch((e) => {
+        throw `${e.response.status}: ${e.response.statusText} for ${e.config.url}`;
+      });
+
+    // console.log(results.data.volumeInfo);
+    response.json(results.data.volumeInfo);
+  } catch (e) {
+    console.error(e);
+  }
+});
 
 module.exports = booksRouter;
